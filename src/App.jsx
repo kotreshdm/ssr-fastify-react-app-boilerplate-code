5e211912cb64f1cf93bbfbc8ae5d16b15b6fd0db
kotresh <47551162+kotreshdm@users.noreply.github.com>
2025-09-12 11:43:42 +0530

    React app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Home from "@/components/Home";
+
+export default function App() {
+  return (
+    <div>
+      <h1>React 19 App</h1>
+      <Home />
+    </div>
+  );
+}
